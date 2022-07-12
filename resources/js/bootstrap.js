b6ff6100f45624d4171d7d0bccef10ab36dadb1f
srmartigan <srmartigan@gmail.com>
2022-07-13 00:59:40 +0200

    configurada

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,5 +30,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
-
-import { Toast, Tooltip, Popover } from 'bootstrap';
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::group(['middleware' => ['verified','auth']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
