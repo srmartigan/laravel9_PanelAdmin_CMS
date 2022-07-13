5f149d9242172f5406fbf2fd62b1367d26259b08
srmartigan <srmartigan@gmail.com>
2022-07-13 23:08:51 +0200

    Creando relaciones entre los modelos User Post Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'user_id',
+        'category_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class)->withDefault();
+    }
+
 }
